359ee137ad8f72fd4918bce94b8798cf679cca0e
Shuraim Issaka <issakashuraim@gmail.com>
2026-02-13 17:39:56 +0000

    fix: import NotificationsModule into PaymentsModule

--- apps/api/src/modules/payments/payments.module.ts
+++ apps/api/src/modules/payments/payments.module.ts
@@ -3,8 +3,10 @@ import { PaymentsController } from "./payments.controller";
 import { WebhooksController } from "./webhooks.controller";
 import { PaymentsService } from "./payments.service";
 import { PaystackService } from "./paystack.service";
+import { NotificationsModule } from "../notifications/notifications.module";
 
 @Module({
+    imports: [NotificationsModule],
     controllers: [PaymentsController, WebhooksController],
     providers: [PaymentsService, PaystackService],
     exports: [PaymentsService],
