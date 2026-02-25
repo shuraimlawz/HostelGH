14c911cb524a311914413538666c08cbcef55d0f
Shuraim Issaka <issakashuraim@gmail.com>
2026-02-25 22:42:08 +0000

    chore: remove Expo/react mobile app and clean tsconfig

--- apps/mobile/metro.config.js
+++
@@ -1,6 +0,0 @@
-// Learn more: https://docs.expo.dev/guides/monorepos
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = config;
