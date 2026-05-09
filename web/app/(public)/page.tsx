f4595b76363202d243f498493f8e7fa33f436b90
Shuraim Issaka <issakashuraim@gmail.com>
2026-05-09 00:10:52 +0000

    chore: hide MarketLeaders row, will revisit later

--- web/app/(public)/page.tsx
+++ web/app/(public)/page.tsx
@@ -3,13 +3,13 @@ import CityCarousel from "@/components/home/CityCarousel";
 import FeaturedHostels from "@/components/home/FeaturedHostels";
 import HomeDiscovery from "@/components/home/HomeDiscovery";
 import HowItWorks from "@/components/home/HowItWorks";
-import MarketLeaders from "@/components/home/MarketLeaders";
+// import MarketLeaders from "@/components/home/MarketLeaders"; // TODO: re-enable when logos are finalised
 
 export default function LandingPage() {
     return (
         <div className="pb-20">
             <HeroSearch />
-            <MarketLeaders />
+            {/* <MarketLeaders /> */}
             <HowItWorks />
 
             <div className="container mx-auto px-4 md:px-10 mt-8">
